20e47af92d1d18cf2094a2747f5369dd22e8ad92
LuisNorman <lanorman14@gmail.com>
2020-07-22 18:11:47 -0500

    Created models, views, urls, and more.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User  # so we can get a reference of who's logged in and rated the movie
+from django.core.validators import MaxValueValidator, \
+    MinValueValidator  # use to validate that the rating is within the max and min
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, related_name='movie', on_delete=models.CASCADE)
+    user = models.ForeignKey(Movie, related_name='user', on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        unique_together = (('user', 'movie'),)  # users cannot rate movies more than once
+        index_together = (('user', 'movie'),)
--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from .views import MovieViewSet, RatingViewSet
 
 router = routers.DefaultRouter()  # allows us to have the standard rest routing urls (put, post, get, delete)
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
